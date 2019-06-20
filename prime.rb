a2ffb343acbb0ba439b3fc3ddb411908593d9715
Jordan Bacon <jmbacon92@gmail.com>
2019-06-20 09:22:01 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,20 +1,3 @@
-def prime?()
-  if n <= 1:
-    return False
-  if n == 2:
-    return True
-  if n == 3:
-    return True
-  if n % 2 == 0:
-    return False
-  if n % 3 == 0:
-    return False
-
-i = 5
-w = 2
-while i * i <= n:
-  if n % i == 0:
-    return False
-  i += w
-  w = 6 - w
-return True
+def prime?(n)
+  (2..n/2).none?{|i| n % i == 0}
+end

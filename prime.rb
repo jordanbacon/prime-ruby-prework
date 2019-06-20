f29721df2fe69e7232ddccc91a5c9ebe8e3c022b
Jordan Bacon <jmbacon92@gmail.com>
2019-06-20 09:14:08 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,21 @@
 def prime?()
+  if n <= 1:
+    return False
+  if n == 2:
+        return True
+    if n == 3:
+        return True
+    if n % 2 == 0:
+        return False
+    if n % 3 == 0:
+        return False
+
+    i = 5
+    w = 2
+    while i * i <= n:
+        if n % i == 0:
+            return False
+        i += w
+        w = 6 - w
+
+    return True
